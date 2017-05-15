022754a91ca3e6ac0b73c676e6581d781f2ddf5a
bianlanzhou <bianlanzhou@gmail.com>
2017-05-15 19:43:49 +0800

    commit

---
+++ src/components/login/login.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1 login-form">
+            
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <form id="authentication" method="post" class="login_validator">
+                            <div class="form-group">
+                                <label for="email" class="sr-only"> E-mail</label>
+                                <input type="text" class="form-control  form-control-lg" id="email" name="username"
+                                       placeholder="E-mail">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="sr-only">Password</label>
+                                <input type="password" class="form-control form-control-lg" id="password"
+                                       name="password" placeholder="Password">
+                            </div>
+                            <div class="form-group checkbox">
+                                <label for="remember">
+                                    <input type="checkbox" name="remember" id="remember">&nbsp; Remember Me
+                                </label>
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" value="Sign In" class="btn btn-primary btn-block"/>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
